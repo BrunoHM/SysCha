f90dd520806975a55604205bf9fea1ef35eb6b90
Bruno Murta <brunoh.murta@gmail.com>
2020-11-13 01:28:04 -0300

    Adicionado rota para usuarios

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,11 +3,13 @@ import {Redirect, Route, Router, hashHistory} from 'react-router'
 
 import Index from './inicio/inicio'
 import Pessoa from './pessoa/pessoa'
+import Usuario from './usuario/usuario'
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/inicio' component={Index} />
         <Route path='/pessoas' component={Pessoa} />
+        <Route path='/usuarios' component={Usuario} />
         <Redirect from='*' to='/inicio' />
     </Router>
 )
