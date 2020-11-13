cc425705e8bc29d0d439f65dbd0e9c064cfec3c3
Bruno Murta <brunoh.murta@gmail.com>
2020-11-13 01:20:20 -0300

    Alterado classe do container

--- src/components/inicio/inicio.jsx
+++ src/components/inicio/inicio.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Container} from 'react-bootstrap'
 
-import Menu from '../Menu/Menu'
+import Menu from '../menu/menu'
 import Sobre from '../sobre/sobre'
 
 import './inicio.css'
 
 export default props => (
-    <Container fluid className='p-0 m-0 darkBGa'>
+    <Container fluid className='p-0 m-0 darkBG'>
         <Menu/>
         <Sobre/>
     </Container>
